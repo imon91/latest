cd9b7735a1ce5ed0e5cd37bf4825dc1a08ca1e0b
imon91 <tasnim.hosen.ewu@gmail.com>
2020-07-07 14:32:33 +0600

    afigyjydfghrtghf

--- loginbd/settings.py
+++ loginbd/settings.py
@@ -138,11 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
-STATIC_URL = 'build/static/'
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'todo/build/static')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ORIGIN_WHITELIST = [
