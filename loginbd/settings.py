e667f24c722f182020da87605e110670396381ac
imon91 <tasnim.hosen.ewu@gmail.com>
2020-07-07 16:00:20 +0600

    afigyjydfghrtghrfijf

--- loginbd/settings.py
+++ loginbd/settings.py
@@ -91,8 +91,10 @@ WSGI_APPLICATION = 'loginbd.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+'default':{
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+}
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'logindb',
