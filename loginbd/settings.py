168cd42d896bf9d6ff0ca3acaccbe61522d74ade
imon91 <tasnim.hosen.ewu@gmail.com>
2020-07-07 14:10:22 +0600

    aff

--- loginbd/settings.py
+++ loginbd/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_ROOT =  os.path.join(BASE_DIR, 'todo/build/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'todo/build', 'static')
 STATIC_URL = 'todo/build/static/'
 
 STATICFILES_DIRS =[
